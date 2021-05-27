0e2f73bbb89b42c84249e8ebbb4d1f4957bedf9b
Ashrom0 <61770891+Ashrom0@users.noreply.github.com>
2021-05-27 13:47:19 -0700

    Add files via upload

---
+++ BombTest.java
@@ -0,0 +1,17 @@
+//import org.junit.Test;
+
+public class BombTest {
+
+	public void testWind() {
+		
+	}//end of testWind
+	
+	public void testRelease() {
+		
+	}//end of testRelease
+	
+	public void testError() {
+		
+	}//end of testError
+	
+}
