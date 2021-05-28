bf759f69797662440ef6392ed05adfbc49d12b0f
Ashrom0 <61770891+Ashrom0@users.noreply.github.com>
2021-05-28 10:13:02 -0700

    Update BombTest.java

--- cscd35bomb/Tests/Tests/BombTest.java
+++ cscd35bomb/Tests/Tests/BombTest.java
@@ -15,10 +15,7 @@ class BombTest {
 	public void testRelease() {
 		Bomb bomb = new Bomb(100, 200, 300, 10, null, 0, 0, 0);
 		Coordinates coor = bomb.getReleaseCoordinates();
-		double x = coor.getX();
-		double y = coor.getY();
-		double alt = bomb.getReleaseAltitude();
-		double descSpeed = bomb.getDescentSpeed();
+		
 	}
 
 	@Test
