d02cacbd80a35a0c10f27148cabbe425a77dc839
Ashrom0 <61770891+Ashrom0@users.noreply.github.com>
2021-05-27 22:02:36 -0700

    Add files via upload

---
+++ BombTests.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import Tests.Bomb.*;
+
+class BombTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
