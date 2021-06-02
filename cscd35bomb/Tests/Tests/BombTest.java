642d28e7d3a967236e03c66f2200132c5981bab7
Ashrom0 <61770891+Ashrom0@users.noreply.github.com>
2021-06-01 17:09:48 -0700

    Update BombTest.java

--- cscd35bomb/Tests/Tests/BombTest.java
+++ cscd35bomb/Tests/Tests/BombTest.java
@@ -25,8 +25,8 @@ public class BombTest {
 		//making sure that X, Y, Release Alt, and Descent Speed are correct.
 		assertEquals(coor.getX(), 100, "getX()");
 		assertEquals(coor.getY(), 200, "getY()");
-		assertEquals(bomb.getReleaseAltitude(), 300, "getReleaseAltitude()");
-		assertEquals(bomb.getDescentSpeed(), 10, "getDescentSpeed()");
+		assertEquals(bomb.getReleaseAltitude(), 300);
+		assertEquals(bomb.getDescentSpeed(), 10);
 		
 	}
 
