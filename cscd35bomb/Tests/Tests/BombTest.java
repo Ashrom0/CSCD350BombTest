eda62d66b326700e3afae840b61376610cc13b54
amiericanderson <ganderson5@eagles.ewu.edu>
2021-06-01 18:28:28 -0700

    Added main as well as features for behavioral tests

--- cscd35bomb/Tests/Tests/BombTest.java
+++ cscd35bomb/Tests/Tests/BombTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.RepeatedTest;
 import org.testng.annotations.Test;
 //import Tests.Tests.Bomb.*;
 
+//import org.junit.jupiter.api.*;
+
 public class BombTest {
 
-	@RepeatedTest(10)
+	@Test
 	public void testWind() {
 		Bomb bomb = new Bomb(0,0,0,0,null,0,45.0,10.0);
 		Assertions.assertEquals(45.0, bomb.getWindDirection());
@@ -38,11 +40,75 @@ public class BombTest {
 
 		assertEquals(bomb1.getErrorType(), Bomb.E_ErrorType.NONE);
 		assertEquals(bomb2.getErrorType(), Bomb.E_ErrorType.UNIFORM);
-		assertEquals(bomb1.getErrorType(), Bomb.E_ErrorType.GAUSSIAN);
+		assertEquals(bomb3.getErrorType(), Bomb.E_ErrorType.GAUSSIAN);
 
 		assertEquals(bomb1.getErrorRange(), 10.0);
 		assertEquals(bomb2.getErrorRange(), 10.0);
 		assertEquals(bomb3.getErrorRange(), 10.0);
 	}
 
+	public static void main(String[] args) {
+		noErrorNoWind();
+		noErrorWithWind();
+		uniformErrorNoWind();
+		gaussianErrorNoWind();
+		uniformErrorWithWind();
+		gaussianErrorWithWind();
+	}
+
+	public static void noErrorNoWind () {
+		System.out.println("No Error No Wind");
+		for (int i = 0; i < 100; i++) {
+			Bomb bomb = new Bomb(200, 300, 1500, 100, Bomb.E_ErrorType.NONE, 0, 0, 0);
+			Bomb.Coordinates coor = bomb.drop();
+
+			System.out.println("" + coor.getX() + ", " + coor.getY());
+		}
+
+	}
+	public static void noErrorWithWind () {
+		System.out.println("No Error With Wind");
+		for (int i = 0; i < 100; i++) {
+			Bomb bomb = new Bomb(200, 300, 1500, 100, Bomb.E_ErrorType.NONE, 0, 60, 25);
+			Bomb.Coordinates coor = bomb.drop();
+
+			System.out.println("" + coor.getX() + ", " + coor.getY());
+		}
+	}
+	public static void uniformErrorNoWind () {
+		System.out.println("Uniform Error No Wind");
+		for (int i = 0; i < 100; i++) {
+			Bomb bomb = new Bomb(200, 300, 1500, 100, Bomb.E_ErrorType.UNIFORM, 150, 0, 0);
+			Bomb.Coordinates coor = bomb.drop();
+
+			System.out.println("" + coor.getX() + ", " + coor.getY());
+		}
+	}
+	public static void gaussianErrorNoWind () {
+		System.out.println("Gaussian Error No Wind");
+		for (int i = 0; i < 100; i++) {
+			Bomb bomb = new Bomb(200, 300, 1500, 100, Bomb.E_ErrorType.GAUSSIAN, 150, 0, 0);
+			Bomb.Coordinates coor = bomb.drop();
+
+			System.out.println("" + coor.getX() + ", " + coor.getY());
+		}
+	}
+	public static void uniformErrorWithWind () {
+		System.out.println("Uniform Error With Wind");
+		for (int i = 0; i < 100; i++) {
+			Bomb bomb = new Bomb(200, 300, 1500, 100, Bomb.E_ErrorType.UNIFORM, 150, 60, 25);
+			Bomb.Coordinates coor = bomb.drop();
+
+			System.out.println("" + coor.getX() + ", " + coor.getY());
+		}
+	}
+	public static void gaussianErrorWithWind () {
+		System.out.println("Gaussian Error With Wind");
+		for (int i = 0; i < 100; i++) {
+			Bomb bomb = new Bomb(200, 300, 1500, 100, Bomb.E_ErrorType.GAUSSIAN, 150, 60, 25);
+			Bomb.Coordinates coor = bomb.drop();
+
+			System.out.println("" + coor.getX() + ", " + coor.getY());
+		}
+	}
 }
